99a41674777aae1a293e0d1fddb370e51ee8019f
Louis Chatriot <louis.chatriot@gmail.com>
2013-03-15 09:27:33 +0100

    Ready to test the module

---
+++ index.js
@@ -0,0 +1,44 @@
+/**
+ * Flatten the given `arr`
+ * Taken from Express
+ */
+function flatten (arr, ret){
+  var len = arr.length
+    , i;
+  ret = ret || [];
+  for (i = 0; i < len; ++i) {
+    if (Array.isArray(arr[i])) {
+      flatten(arr[i], ret);
+    } else {
+      ret.push(arr[i]);
+    }
+  }
+  return ret;
+}
+
+
+/**
+ * Call middleware before every every route handler in this groupe. Use like this:
+ * beforeEach(expressApp, middleware, function(app) {
+ *   app.get('/route1', handler1);
+ *   app.post('/route2', handler2);
+ * });
+ */
+module.exports.beforeEach = function () {
+  var expressApp = arguments[0]
+    , callback = arguments[arguments.length - 1]
+    , beforeHandlers = flatten([].slice.call(arguments, 1, arguments.length - 1)) || []
+    , fakeExpressApp = {}
+    ;
+
+  if (!expressApp) { return; }
+
+  supportedHttpMethods.forEach(function (method) {
+    fakeExpressApp[method] = function(path) {
+      expressApp[method].call(expressApp, path, beforeHandlers, [].slice.call(arguments, 1));
+    };
+  });
+
+  callback(fakeExpressApp);
+};
+
---
+++ test/tests.js
@@ -0,0 +1,13 @@
+var should = require('chai').should()
+  , assert = require('chai').assert
+  ;
+
+
+describe('#timeago', function () {
+	it('Should return just ago if span is under a minute', function () {
+		var test;
+
+	});
+
+});
+
